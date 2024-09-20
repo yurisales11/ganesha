a02bb0cb8784afb49efdfadc96ac24347fbc7c37
yurisales11 <135793297+yurisales11@users.noreply.github.com>
2024-09-20 19:59:51 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ganesha',
+  webDir: 'www'
+};
+
+export default config;
